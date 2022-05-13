e9fdbe110e0232ab0933b4f3c2aae3cefdec3958
T0mmy <antchupinin@gmail.com>
2022-05-13 23:37:07 +0300

    Реализована функция isOperator, которая определяет является ли строка оператором в формате Си-выражений

--- SoftQA_Chupinin_11/ConvertToTEX.cpp
+++ SoftQA_Chupinin_11/ConvertToTEX.cpp
@@ -1,2 +1,28 @@
 #include "ConvertToTEX.h"
 
+
+int isOperator(const string& str)
+{
+    int numberOfOperands = -1;
+
+    const vector<string> oneOperand = { "!", "all()", "exist()" };
+
+    const vector<string> twoOperands = { "+", "-", "/", "*", "+-", "-+", "%", "=", "!=", ">=", "<=", ">", "<",
+         "&&", "||", ",", "..", "&", "|", "pow()", "sqrt()", "in()", "noin()", "_", "frac()" };
+
+    const vector <string> threeOperands = { "summator()" };
+
+    for (int i = 0; i < oneOperand.size(); i++)
+        if (str == oneOperand[i])
+            numberOfOperands = 1;
+
+    for (int i = 0; i < twoOperands.size(); i++)
+        if (str == twoOperands[i])
+            numberOfOperands = 2;
+
+    for (int i = 0; i < threeOperands.size(); i++)
+        if (str == threeOperands[i])
+            numberOfOperands = 3;
+
+    return numberOfOperands;
+}
