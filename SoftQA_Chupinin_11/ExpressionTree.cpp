0aee56e35ee22acd74e4081d5a711d3d1344bad9
T0mmy <antchupinin@gmail.com>
2022-05-14 01:06:22 +0300

    Добавлены методы для ExpressionTree

--- SoftQA_Chupinin_11/ExpressionTree.cpp
+++ SoftQA_Chupinin_11/ExpressionTree.cpp
@@ -35,3 +35,66 @@ ExpressionTree::ExpressionTree(const string value)
 		this->expressionElementType = UNDEFINED;
 	}
 }
+
+string ExpressionTree::getValue()
+{
+	return this->value;
+}
+
+int ExpressionTree::getOperandsCount()
+{
+	return this->operandsCount;
+}
+
+int ExpressionTree::getOperatorPriority()
+{
+	return this->operatorPriority;
+}
+
+ExpressionTree* ExpressionTree::getChild(int number)
+{
+	if (number > MAX_OPERAND_COUNT)
+		return NULL;
+
+	return this->children[number];
+}
+
+vector <ExpressionTree*> ExpressionTree::getChildren()
+{
+	return children;
+}
+
+
+
+ExpressionElementType ExpressionTree::getExpressionElementType()
+{
+	return this->expressionElementType;
+}
+
+string ExpressionTree::getTexFormat()
+{
+	return this->texFormat;
+}
+
+
+void ExpressionTree::addChild(ExpressionTree* child)
+{
+	this->children.push_back(child);
+}
+
+void ExpressionTree::addChild(string value)
+{
+
+	ExpressionTree* child = new ExpressionTree(value);
+	this->addChild(child);
+
+}
+
+void ExpressionTree::deleteTree()
+{
+	for (int i = 0; i < this->operandsCount; i++) {
+		children[i]->deleteTree();
+	}
+
+	delete this;
+}
