e130c1c816243e2ef3ebedc69b2c331e4cb54511
T0mmy <antchupinin@gmail.com>
2022-05-20 18:10:38 +0300

    добавлены комментарии для функции convertReversePolishEntryToTree

--- SoftQA_Chupinin_11/convertTreeToTEX.cpp
+++ SoftQA_Chupinin_11/convertTreeToTEX.cpp
@@ -2,22 +2,22 @@
 
 ExpressionTree* convertReversePolishEntryToTree(vector<string>& reversePolishEntryElements)
 {
-	int lastElementNumber = reversePolishEntryElements.size() - 1;
-	string value = reversePolishEntryElements[lastElementNumber];
-	reversePolishEntryElements.pop_back();
+	int lastElementNumber = reversePolishEntryElements.size() - 1; // номер последнего элемента в обратной польской записи
+	string value = reversePolishEntryElements[lastElementNumber];  // последний элемент обратной польской запии
+	reversePolishEntryElements.pop_back(); // удаляем элемент в конце вектора
 
-	ExpressionTree* current = new ExpressionTree(value);
+	ExpressionTree* current = new ExpressionTree(value); // создаем новую вершину в наше дерево
 
 
-	if (current->getExpressionElementType() == OPERATOR && reversePolishEntryElements.size() >= current->getOperandsCount()) {
+	if (current->getExpressionElementType() == OPERATOR && reversePolishEntryElements.size() >= current->getOperandsCount()) { // новая вершина является оператором и количество элементов в векторе достаточно, чтобы заполнить их операндами для данной операции
 
-		vector<ExpressionTree*> operands(current->getOperandsCount());
-		for (int i = current->getOperandsCount() - 1; i >= 0; i--) {
-			auto p = convertReversePolishEntryToTree(reversePolishEntryElements);
-			operands[i] = p;
+		vector<ExpressionTree*> operands(current->getOperandsCount()); // определяем количество операндов оператора
+		for (int i = current->getOperandsCount() - 1; i >= 0; i--) { // для всех операндов оператора
+			auto p = convertReversePolishEntryToTree(reversePolishEntryElements); // находим операнд оператора
+			operands[i] = p; 
 		}
 
-		for (int i = 0; i < operands.size(); i++) {
+		for (int i = 0; i < operands.size(); i++) { // добавляем операнд оператору
 			current->addChild(operands[i]);
 		}
 	}
@@ -25,11 +25,6 @@ ExpressionTree* convertReversePolishEntryToTree(vector<string>& reversePolishEnt
 	{
 		// выкидываем исключение
 	}
-
-	if (current->getValue() == "/") {
-		cout << "YES";
-	}
-	//cout << current->getValue() << "\n";
-
+	
 	return current;
 }
