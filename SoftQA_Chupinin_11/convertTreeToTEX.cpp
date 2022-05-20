730c2033cb6649c158387c13eb859ae8e41ba2d5
T0mmy <antchupinin@gmail.com>
2022-05-20 23:02:35 +0300

    Реализована функция convertSubFormulaToTex, которая переводит подформулу в tex-формат, используя алгоритм обхода в глубину.

--- SoftQA_Chupinin_11/convertTreeToTEX.cpp
+++ SoftQA_Chupinin_11/convertTreeToTEX.cpp
@@ -28,3 +28,72 @@ ExpressionTree* convertReversePolishEntryToTree(vector<string>& reversePolishEnt
 	
 	return current;
 }
+
+
+string convertSubFormulaToTex(ExpressionTree* current, int& curPriority)
+{
+
+	string subFormula; // считать, что подстрока не найдены
+
+	if (current->getExpressionElementType() == OPERATOR) { // если проверяемая вершина является оператором
+
+
+		string operands[MAX_OPERAND_COUNT] = {}; // массив операндов
+		int priority[MAX_OPERAND_COUNT]; // массив  приоритета операндов
+
+		for (int i = current->getOperandsCount() - 1; i >= 0; i--) { // для всех операндов оператора
+			operands[i] = convertSubFormulaToTex(current->getChild(i), priority[i]); // находим операнд и его приоритет
+		}
+
+
+		int currentOperandsCount = current->getOperandsCount(); // количество операндов у оператора
+		string operatorTex = current->getTexFormat(); // переводим оператор в tex-формат
+
+		if (currentOperandsCount == 1) { // количество операндов равно  1
+			subFormula = operatorTex + " {" + operands[0] + "}"; // перевести подстроку в tex-формат
+		}
+
+		if (currentOperandsCount == 2) // количество операндов равно 2
+		{
+			string value = current->getValue(); // значение вершины
+
+			if (value == "sqrt()") { // значение является корнем
+				subFormula = operatorTex + "[ " + operands[0] + " ]" + " { " + operands[1] + " }"; // перевести подстроку в tex-формат
+			}
+
+			else if (value == "frac()") { // значение является дробью
+				subFormula = operatorTex + "{ " + operands[0] + " }" + " { " + operands[1] + " }"; // перевести подстроку в tex-формат
+			}
+			else {
+
+				curPriority = current->getOperatorPriority(); // корректный приоритет
+
+				if (curPriority < priority[0]) operands[0] = "(" + operands[0] + ")"; // если приоритет текущей операции выше приоритета операции левого операнда, взять левый операнд в скобки
+				if (curPriority < priority[1]) operands[1] = "(" + operands[1] + ")"; // если приоритет текущей операции выше приоритета операции правого операнда, взять правый операнд в скобки
+
+				if (value == "pow()") // опер
+				{
+					subFormula = operands[0] + " " + operatorTex + " {" + operands[1] + "}";
+				}
+				else
+					subFormula = operands[0] + " " + operatorTex + " " + operands[1];
+			}
+
+		}
+
+		if (currentOperandsCount == 3) { // количество операндов равно 3
+			subFormula = operatorTex + "_ {" + operands[0] + "} ^ {" + operands[1] + "}" + " {" + operands[2] + "}"; // перевести подстроку в tex-формат
+		}
+
+	}
+
+	else if (current->getExpressionElementType() == GREEKLETTER) { // строка является греческой буквой
+		subFormula = current->getTexFormat(); // перевести в TEX-формат
+	}
+	else
+	{
+		subFormula = current->getValue(); // иначе считать, что подформула равна значнию
+	}
+
+	return subFormula;
+}
